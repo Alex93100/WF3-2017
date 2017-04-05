9b972e6b2fc41d76f9ebad2e47cd5a662aef9ae8
Etudiant <Etudiant>
2017-04-05 12:45:22 +0200

    Fonction load() et formulaire jQuery

--- Avril/5avril/exo71/js/script.js
+++ Avril/5avril/exo71/js/script.js
@@ -37,9 +37,47 @@ $(document).ready(function(){
     function submitForm(){
         // Capter la soumission du Foormulaire
         $('form').submit(function(evt){
+            
+            // Créer une variable pour la validation du formulaire
+            var formScore = 0;
+            
+            // Bloquer le comportement naturel du formulaire
             evt.preventDefault();
-
             console.log('Submit du formulaire');
+
+            // Min 4 caractere pour l'email et Min 5 caractere pour le msg
+            if($('[type="email"]').val().length<4){
+                console.log('Email manquant');
+            }
+            else{
+                console.log('Email OK');
+                // Incrémenter formScore
+                formScore++;
+            };
+
+            if($('textarea').val().length<5){
+                console.log('Message Error');
+            }
+            else{
+                console.log('Message OK');
+                formScore++;    
+            };
+
+            // Vérifier la valeur de formScore
+            if(formScore ==2 ){
+                console.log('Le formulaire est validé !');
+
+                // Envoi des données vers le fichier de traitement PHP
+                    //  => le fichier PHP répond true : je peux continuer mon code
+
+                    // Ajouter le msg dans la balise aside
+                    $('aside').append('<h3>' + $('textarea').val() + '</h3><p>' + $('[type="email"]').val() + '</p>');
+                    
+                    // Reset du formulaire
+                    $('form')[0].reset();
+            };
         });
     };
+
+
 }); // Fin du chargmeent du DOM
