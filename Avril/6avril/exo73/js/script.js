de071eab907d3640cbafc2bea33a887b53aac88b
Etudiant <Etudiant>
2017-04-06 13:08:08 +0200

    Application Web

--- Avril/6avril/exo73/js/script.js
+++ Avril/6avril/exo73/js/script.js
@@ -20,14 +20,46 @@ $(document).ready(function(){
             // Ouvrir la modal au click sur les boutons
             $('button').click(function(){
 
-                console.log($(this).prev().text());
-                
+                // Afficher le titre du projet
+                var modalTitle = $(this).prev().text();
+                $('#modal span').text(modalTitle);
+
+                // Afficher l'image du projet
+                var modalImage = $(this).parent().prev().attr('src');
+                $('#modal img').attr('src', modalImage).attr('alt', modalTitle);
+
                 $('#modal').fadeIn();    
             });
 
             // Fermer la modal au click sur .fa-times
             $('.fa-times').click(function(){
                 $('#modal').fadeOut();
+            });
+        };
+
+    /*
+        Formulaire
+    */ 
+        // Créer une fonction pour la gestion du formulaire
+        function contactForm(){
+            
+            // Capter le focus sur les inputs et le textarea
+            $('input, textarea').focus(function(){
+
+                // Sélectioner la balise précédente pour y ajouter la class openedLabel
+                $(this).prev().addClass('openedLabel');
+
+            });
+
+            // Capter le blur sur les input et le textarea
+            $('input,textarea').blur(function(){
+
+                // Vérifier s'il n'y à pas de caractère dans le input
+                if($(this).val().length == 0){
+
+                    // Sélectioner la balise précédente pour supprimer la class openedLabel
+                    $(this).prev().removeClass();
+                };
             });
         };
 
@@ -86,6 +118,13 @@ $(document).ready(function(){
 
                             // Appeler la fonction pour ouvrir la modal
                             openModal();
+                        };
+
+                        // Vérifier si l'utilisateur est sur la page contacts.html
+                        if (viewToLoad == 'contacts.html') {
+                            
+                            //Déclancher la fonction de gestion du formulaire
+                            contactForm(); 
                         };
                     });
                 });
