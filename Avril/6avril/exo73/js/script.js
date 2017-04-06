c324ff107c3d56fabf449a73ee6b7657ff8100f8
Etudiant <Etudiant>
2017-04-06 15:40:21 +0200

    Application Web

--- Avril/6avril/exo73/js/script.js
+++ Avril/6avril/exo73/js/script.js
@@ -61,7 +61,96 @@ $(document).ready(function(){
                     $(this).prev().removeClass();
                 };
             });
+
+            //Capter la soumission de mon formulaire
+            $('form').submit(function(evt){
+
+                // Bloquer le comportement naturel du formulaire
+                evt.preventDefault();
+
+                // Définir les variables globales du formulaire
+                var userName = $('#userName');
+                var userEmail = $('#userEmail');
+                var userSubject = $('#userSubject');
+                var userMessage = $('#userMessage');
+                var checkbox = $('[type="checkbox"]');
+                var formScore = 0;
+
+                // Vérifier que userName à au min 2 caractères
+                if(userName.val().length < 2){
+                    // Afficher un msg d'erreur
+                    $('[for="userName"] b').text('Minimum 2 caractères');
+
+                    // version 2 : userName.prev().children('b').text('Minimum 2 caractères')
+                }
+                else{
+                    // Incrémenter la valeur de formScore
+                    formScore++;
+                };
+
+
+                // Vérifier que userEmail à au moin 5 caractères
+                if(userEmail.val().length < 5){
+                    // Afficher un msg d'erreur
+                    $('[for="userEmail"] b').text('Minimum 5 caractères');
+                }
+                else{
+                    // Incrémenter la valeur de formScore
+                    formScore++;  
+                };
+
+                
+                // Vérifier que l'utilisateurà bien sélectionné un sujet
+                if(userSubject.val() == 'null' ){
+                    // Afficher un msg d'erreur
+                    $('[for="userSubject"] b').text('select obligatoire');
+                }
+                else{
+                    // Incrémenter la valeur de formScore
+                    formScore++;     
+                };
+
+
+                // Vérifier que userMessage à au moin 10 caractères
+                if(userMessage.val().length < 10){
+                    // Afficher un msg d'erreur
+                    $('[for="userMessage"] b').text('Minimum 10 caractères');
+                }
+                else{
+                    // Incrémenter la valeur de formScore
+                    formScore++;  
+                };
+
+                // Vérifier que le CG est accepter
+                if(checkbox[0].checked == false){
+                    // Afficher un msg d'erreur
+                    $('form p b').text('Vous devez accepter la CG');
+                }
+                else{
+                    // Incrémenter la valeur de formScore
+                    formScore++;      
+                };
+
+                // Validation final du formulaire
+                if(formScore == 5){
+
+                    console.log('Le formulaire est validé')
+
+                    // Envoi des données dans le fichier de traitement PHP
+                    // Php répond true => continuer le traitement du formulaire
+
+                        // Vider les champs du formulaire
+                        $('form')[0].reset();
+
+                        // Replacer les label
+                        $('label').removeClass();
+                        
+                };
+
+            });
+
         };
+        
 
     // Charger le contenu de home.html dans le main
     $('main').load('views/home.html');
