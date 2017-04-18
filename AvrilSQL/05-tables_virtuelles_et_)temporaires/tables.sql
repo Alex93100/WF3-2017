f7b612ae862b2f9d7ea11f5430a992b31916672a
Etudiant <Etudiant>
2017-04-18 16:20:40 +0200

    SQL Tables virtuelles

---
+++ AvrilSQL/05-tables_virtuelles_et_)temporaires/tables.sql
@@ -0,0 +1,27 @@
+--***********************************
+-- tables virtuelles : vues
+--***********************************
+
+    -- Les vues(ou tables virtuelles) sont des objets de la base de données, constitué d'un nom, et d'une requête de sélection.
+
+    -- Une fois une vue definie, on peut l'utiliser comme on le ferait avec une table, laquelle serait constituée des données sélectionnées par la requête définissant la vue.
+
+    USE entreprise;
+
+    -- Création d'une vue :
+    CREATE VIEW vue_homme AS SELECT prenom, nom, sexe, service FROM employes WHERE sexe = 'm';
+
+    -- crée une table virtuelle (ou vue) remplie avec kes données du SELECT
+
+    -- Une seconde requête effectuée sur la vue :
+    SELECT prenom FROM vue_homme; -- on peut effectuer toutes les opérations habituelles sur cette table virtuelle vue_homme
+
+    -- Si il y a un changement dans la table d'origne, la vue est corrigée dynamiquement car elle enregiste la requete SELECT qui pointe vers la table d'origine. Inversement, si il y a un changement dans la table virtuelle, il s'impacte dans la table d'origine.
+
+    -- Il ya intérêt à faire des vue en termes de gain de ressources, ou quand il y a des requête complexxes à exécuter.
+
+    SHOW TABLES; -- cette vue est visible dans la liste des tables avec cette instruction
+
+    --  Supprimer une vue :
+
+    DROP VIEW vue_homme;
