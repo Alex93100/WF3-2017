b99d4798c2b32614b497f92c3ca54acc775f3ac4
Etudiant <Etudiant>
2017-04-18 17:09:53 +0200

    SQL Variables

---
+++ AvrilSQL/07-variables/variables.sql
@@ -0,0 +1,9 @@
+--********************************
+-- Les variables en SQL
+--********************************
+    -- Une varaible est un espace mémoire nommé qui permet de conserver une valeur.
+
+    -- permet d'observer les variables systèmes :
+    SHOW VARIABLES;
+
+    SELECT @@version; -- on met deux @ pour accéder à une variable système
