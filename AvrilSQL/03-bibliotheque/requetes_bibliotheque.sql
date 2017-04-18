d40d9b27e7e74df1d269e592cf4012341d6165f3
Etudiant <Etudiant>
2017-04-18 14:52:30 +0200

    SQL BDD Bibliotheque

--- AvrilSQL/03-bibliotheque/requetes_bibliotheque.sql
+++ AvrilSQL/03-bibliotheque/requetes_bibliotheque.sql
@@ -95,8 +95,108 @@ USE bibliotheque;
 -- Jointures internes
 -- **************************************
 
--- Différence entre une jointure et une requête imbriquée : une requête imbriquée est possible seulement dans le cas ou les champs affichés (ceux qui sont dans le SELECT) sont issus de la même table.
+    -- Différence entre une jointure et une requête imbriquée : une requête imbriquée est possible seulement dans le cas ou les champs affichés (ceux qui sont dans le SELECT) sont issus de la même table.
 
--- Avec une requête de jointure, les champs sélectionnées peuvent être issus de tables difféerentes. Une jointure est une requête premettant de faire des relations entre les différentes tables afin d'avoir des colonnes ASSOCIEES qui ne forme qu'UN SEUL résultat.
+    -- Avec une requête de jointure, les champs sélectionnées peuvent être issus de tables difféerentes. Une jointure est une requête premettant de faire des relations entre les différentes tables afin d'avoir des colonnes ASSOCIEES qui ne forme qu'UN SEUL résultat.
 
--- Afficher les dates de sortue et e rendu pour l'abonné Guillaume :
+    -- Afficher les dates de sortue et e rendu pour l'abonné Guillaume :
+    SELECT a.prenom, e.date_sortie, e.date_rendu
+    FROM abonne a
+    INNER JOIN emprunt e
+    ON a.id_abonne = e.id_abonne
+    WHERE a.prenom = 'guillaume';
+
+    -- 1e ligne : ce que je souhaite Afficher
+    -- 2e ligne : la 1ere table d'ou proviennent les informations
+    -- 3e ligne : la seconde table d'ou proviennent les informations
+    -- 4e ligne : la jointure qui lie les 2 tables avec le champ COMMUN
+    -- 5e ligne : la ou les conditions supplémentaires
+
+    -- Exercices : Nous aimerons connaitre les mouvements des livres (titre, date_sortie et date_rendu) ecrits par Alphonse Daudet :
+
+    SELECT l.titre, e.date_sortie, e.date_rendu
+    FROM livre l
+    INNER JOIN emprunt e
+    ON l.id_livre = e.id_livre
+    WHERE l.auteur = 'Alphonse Daudet';
+
+    -- Exercices : qui a emprunté "Une Vie" sur l'année 2011 ?
+
+    SELECT a.prenom, a.id_abonne
+    FROM abonne a
+    INNER JOIN emprunt e
+    ON a.id_abonne = e.id_abonne
+    INNER JOIN livre l
+    ON e.id_livre = l.id_livre    
+    WHERE l.titre = 'Une vie' AND e.date_sortie BETWEEN '2011-01-01' AND '2011-12-31';
+    
+    -- Exercices : Afficher le nombre de livres empruntés par chaque abonné :
+
+    SELECT a.prenom, COUNT(e.id_emprunt) AS nombre
+    FROM abonne a
+    INNER JOIN emprunt e
+    ON a.id_abonne = e.id_abonne
+    GROUP BY prenom;
+
+    -- Exercices : Afficher qui empruntés quels livres et a quelles date de sortie ? (prenom, date_sortie, titre) :
+
+    SELECT a.prenom, l.titre, e.date_sortie
+    FROM abonne a
+    INNER JOIN emprunt e
+    ON a.id_abonne = e.id_abonne
+    INNER JOIN livre l
+    ON e.id_livre = l.id_livre;
+    -- Ici pas de GROUP BY car il est normal que les prénoms sortent plusieurs fois (ils peuvent emprunter plusieurs livres)
+
+    -- Afficher les prenoms des abonnes avec les id_livres qu'ils ont empruntés :
+
+    SELECT a.prenom, e.id_livre
+    FROM abonne a
+    INNER JOIN emprunt e
+    ON a.id_abonne = e.id_abonne;
+
+
+
+
+-- **************************************
+-- Jointures externe
+-- **************************************
+    -- Une jointure externe permet de faire des requêtes sans correspondance exigée entre les valeurs requêtées.
+
+    -- Ajoutez vous dans la table abonne :
+    INSERT INTO abonne (prenom) VALUES ('Alexandre');
+
+    -- Si on relance la dernière requête de jointure interne, nous n'apparaissons pas dans la liste car nous n'avons pas emprunté de livre.
+    -- Pour y remédier, noujs faisons une jointure externe :
+
+    SELECT a.prenom, e.id_livre
+    FROM abonne a
+    LEFT JOIN emprunt e
+    ON a.id_abonne = e.id_abonne;
+
+    -- La clause LEFT JOIN permet de rapatrier TOUTES les données dans la table considérée comme étant  gauche de l'instruction LEFT JOIN (donc abonne dans notre cas), sans correspondance exigée dans l'autre table (eemprunt ici).
+
+    -- Voici le cas avec un livre supprimé de la bibliothèque :
+    DELETE FROM livre WHERE id_livre = 100;
+
+    -- On visualise les emprunts avec une jointure interne :
+    SELECT emprunt.id_emprunt, livre.titre
+    FROM emprunt
+    INNER JOIN livre
+    ON emprunt.id_livre = livre.id_livre;
+    --  On ne voit pas dans cette requête le livre "Une Vie" qui a été supprimé.
+
+    -- On fais la même chose  avec une jointure externe :
+    SELECT emprunt.id_emprunt, livre.titre
+    FROM emprunt
+    LEFT JOIN livre
+    ON emprunt.id_livre = livre.id_livre;
+    -- Ici tous les emprunts sont affichés, y compris ceux pour lesquels les titres sont supprimés et remplacé par NULL.
+
+
+
+-- **************************************
+-- UNION
+-- **************************************
+
+    -- Union permet de fusionner le résultat
