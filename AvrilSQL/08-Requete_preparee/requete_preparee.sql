18566f2eee7d6717640ab6530461eebadd0f4565
Etudiant <Etudiant>
2017-04-19 10:00:51 +0200

    SQL Requête préparée

--- AvrilSQL/08-Requete_preparee/requete_preparee.sql
+++ AvrilSQL/08-Requete_preparee/requete_preparee.sql
@@ -14,4 +14,11 @@
 
         -- 1° Préparation :
         PREPARE req FROM "SELECT * FROM employes WHERE service = 'commercial'";  -- déclarer une requête préparée
-        
+
+        -- 2° Exécution de la requête :
+        EXECUTE req;  -- On peut exécuter la requête plusieur fois sans refaire le cycle analyse / interprétation => gain de performance
+
+
+    -- Requête préparée avec un marqueur "?" :
+        -- 1° Préparation :
+        PREPARE req2 FROM "SELECT * FROM employes WHERE prenom = ?";  -- déclarer une requête préparée
