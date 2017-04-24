bd72035353c6ef488c08f219d080449761312b4d
Etudiant <Etudiant>
2017-04-24 16:06:42 +0200

    PHP - Exo

---
+++ PHP1erePartie/04-exercices/fonction.inc.php
@@ -0,0 +1,14 @@
+<?php
+// Voici une fonction qui permet de retoruner le prix d'un fruit en fonction d'un poids
+
+function calcul ($fruit, $poids){
+    switch($fruit){
+        case 'cerises' : $prix_kg = 5.76; break;
+        case 'bananes' : $prix_kg = 1.09; break;
+        case 'pommes' : $prix_kg = 1.61; break;
+        case 'peches' : $prix_kg = 3.23; break;
+        default : return 'Fruit inexistant';
+    }
+
+    $resultat = $poids * $prix_kg / 1000; // calcule le prix en total selon un poids donnée en grammes
+}
