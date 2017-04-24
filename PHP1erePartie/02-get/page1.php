914ec898cc63eaad984d94764064583aa34ef9b4
Etudiant <Etudiant>
2017-04-24 11:51:04 +0200

    PHP - get

--- PHP1erePartie/02-get/page1.php
+++ PHP1erePartie/02-get/page1.php
@@ -1,1 +1,7 @@
 <h1>Nos produits :</h1>
+
+<a href="page2.php?article=jean&couleur=bleu&prix=10">Jean bleu</a>
+<br>
+<a href="page2.php?article=robe&couleur=rouge&prix=20">Robe rouge</a>
+<br>
+<a href="page2.php?article=pull&couleur=blanc&prix=30">Pull blanc</a>
