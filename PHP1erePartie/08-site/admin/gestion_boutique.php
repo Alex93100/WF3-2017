f7fe0d96af2fc2a9599478c26c0f792200f80678
Etudiant <Etudiant>
2017-05-02 16:42:33 +0200

    PHP - Site

--- PHP1erePartie/08-site/admin/gestion_boutique.php
+++ PHP1erePartie/08-site/admin/gestion_boutique.php
@@ -10,6 +10,22 @@
             exit();
         }
 
+    // 7- Suppression d'un produit
+        if(isset($_GET['action']) && $_GET['action'] == 'suppression' && isset($_GET['id_produit'])){
+            // On sélectionne en base la photo pour pouvoir supprimer le fichier pjoto correspondant :
+            $resultat = executeRequete("SELECT photo FROM produit WHERE id_produit = :id_produit", array(':id_produit' => $_GET['id_produit']));
+            $produit_a_supprimer = $resultat->fetch(PDO::FETCH_ASSOC); // pas de while car qu'un seul produit
+            $chemin_photo_a_supprimer = $_SERVER['DOCUMENT_ROOT'] . $produit_a_supprimer['photo']; //Chemin du fichier à supprimer
+            if(!empty($produit_a_supprimer['photo']) && file_exists($chemin_photo_a_supprimer)){
+                // si il y a un chemin de photo en base ET que le fichier existe, on peut le supprimer :
+                unlink($chemin_photo_a_supprimer); // supprime le fichier spécifié
+            }
+            // Puis suppression du produit en BDD :
+            executeRequete("DELETE FROM produit WHERE id_produit = :id_produit", array(':id_produit' => $_GET['id_produit']));
+            $contenu .= '<div class="bg_success">Le produit a été supprimé !</div>';
+            $_GET['action'] = 'affichage'; // Pour lancer l'affichage de sproduit dans le tableau HTML (point 6)
+        }
+
     // 4- Enregistrement du produit en BDD
 
         if($_POST){ // Equivalent à !empty($_POST) car si le $_POST est rempli, il vaut TRUE = formulaire posté
@@ -36,17 +52,13 @@
                 // Enregistrement du fichier photo sur le serveur :
                 copy($_FILES['photo']['tmp_name'], $photo_dossier);
                 // On copie le fichier temporaire de la photo stockée au chemin indiqué par $_FILES['photo']['tmp_name'] dans le chemin $photo_dossier de notre serveur
-                
-
             }
-
-            
+   
             // 4- Suite de l'enregistrement en BDD :
             executeRequete("REPLACE INTO produit (id_produit, reference, categorie, titre, description, couleur, taille, public, photo, prix, stock)VALUES(:id_produit, :reference, :categorie, :titre, :description, :couleur, :taille, :public, :photo_bdd, :prix, :stock)", array('id_produit' => $_POST['id_produit'], 'reference' => $_POST['reference'], 'categorie' => $_POST['categorie'], 'titre' => $_POST['titre'], 'description' => $_POST['description'], 'couleur' => $_POST['couleur'], 'taille' => $_POST['taille'], 'public' => $_POST['public'], ':photo_bdd' => $photo_bdd, 'prix' => $_POST['prix'], 'stock' => $_POST['stock']));
 
             $contenu .='<div class="bg-success">Le produit a été enregistré</div>';
             $_GET['action'] = 'affichage'; // On met la valeur 'affichage' dans $_GET['action'] pour affcher automatiquement la table HTML des produits plus loin dans le script (point 6)
-
         } 
 
     // 2- Les liens "affichage" et "ajout d'un produit" :
@@ -55,8 +67,7 @@
                         <li><a href="?action=affichage">Affichage du produits</a></li>
                         <li><a href="?action=ajout">Ajout d\'un produits</a></li>
                     </ul>';
-
-    
+  
     // 6- Affichage des produits dans le back-office :
         if(isset($_GET['action']) && $_GET['action'] == 'affichage' || !isset($_GET['action'])) {
             // Si $_GET contient affichage ou que l'on arrive sur l apage la 1ere fois ($_GET['action'] n'existe pas)
@@ -88,24 +99,14 @@
                                 $contenu .= '<td>' . $data . '</td>';
                             }
                         }
+                        $contenu .= '<td>
+                                        <a href="?action=modification&id_produit='. $ligne['id_produit'] .'">modifier</a> /
+                                        <a href="?action=suppression&id_produit='. $ligne['id_produit'] .'"onclick="return(confirm(\'Etes-vous certain de vouloir supprimer ce produit ?\'));">supprimer</a>
+                                    </td>';
                     $contenu .= '</tr>';
                 }
-
             $contenu .= '</table>';
-            
-            
-           
         }
-
-
-
-
-
-
-
-
-
-
     //--------------------------- AFFICHAGE ------------------------------
 
     require_once('../inc/haut.inc.php');
@@ -115,26 +116,39 @@
 
         if(isset($_GET['action']) && ($_GET['action'] == 'ajout' || $_GET['action'] == 'modification')) :
         // Si on a demandé l'ajout d'un produit ou sa modification, on affiche le formulaire :
+
+            // 8- Formulaire de modification avec présaisie des infos dans le formulaire :
+            if(isset($_GET['id_produit'])){
+                // Pour préremplir le formulaire, on requête en BDD les infos du produit passé dans l'URL:
+                $resultat = executeRequete("SELECT * FROM produit WHERE id_produit = :id_produit", array(':id_produit' => $_GET['id_produit']));
+
+                $produit_actuel= $resultat->fetch(PDO::FETCH_ASSOC); // pas de while car un seul produit
+            }
+        
+
+        
+
+
 ?>
 
 <h3>Formulaire d'ajout ou de modification d'un produit</h3>
 <form method="post" enctype="multipart/form-data" action=""> <!-- "multipart/form-data" perùet d'uploader un fichier et de générer une superglobale $_FILES -->
-    <input type="hidden" id="id_produit" name="id_produit" value="0"> <!-- champ caché qui récetionne l'id_produit nécessaire lors de la modification d'un produit existant -->
+    <input type="hidden" id="id_produit" name="id_produit" value="<?php echo $produit_actuel['id_produit']?? 0;?>"> <!-- champ caché qui récetionne l'id_produit nécessaire lors de la modification d'un produit existant -->
     
     <label for="reference">Référence</label><br>
-    <input type="text" id="reference" name="reference" value=""><br><br>
+    <input type="text" id="reference" name="reference" value="<?php echo $produit_actuel['reference']?? '';?>"><br><br>
 
     <label for="categorie">Catégorie</label><br>
-    <input type="text" id="categorie" name="categorie" value=""><br><br>
+    <input type="text" id="categorie" name="categorie" value="<?php echo $produit_actuel['categorie']?? '';?>"><br><br>
 
     <label for="titre">Titre</label><br>
-    <input type="text" id="titre" name="titre" value=""><br><br>
+    <input type="text" id="titre" name="titre" value="<?php echo $produit_actuel['titre']?? '';?>"><br><br>
 
     <label for="description">Description</label><br>
-    <textarea id="description" name="description" value=""></textarea><br><br>
+    <textarea id="description" name="description"><?php echo $produit_actuel['description']?? '';?></textarea><br><br>
 
     <label for="couleur">Couleur</label><br>
-    <input type="text" id="couleur" name="couleur" value=""><br><br>
+    <input type="text" id="couleur" name="couleur" value="<?php echo $produit_actuel['couleur']?? '';?>"><br><br>
 
     <label>Taille</label><br>
     <select name="taille">
@@ -154,10 +168,10 @@
     
 
     <label for="prix">Prix</label><br>
-    <input type="text" id="prix" name="prix" value=""><br><br>
+    <input type="text" id="prix" name="prix" value="<?php echo $produit_actuel['prix']?? 0;?>"><br><br>
 
     <label for="stock">Stock</label><br>
-    <input type="text" id="stock" name="stock" value=""><br><br>
+    <input type="text" id="stock" name="stock" value="<?php echo $produit_actuel['stock']?? 0;?>"><br><br>
 
     <input type="submit" value="valider" class="btn"><br><br>
 
@@ -165,9 +179,5 @@
 <?php
         endif;
 
-
-
     require_once('../inc/bas.inc.php');
-
-
 ?>
