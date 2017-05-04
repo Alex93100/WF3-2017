a922f726ca3358345266176322db527ccc670204
Etudiant <Etudiant>
2017-05-04 17:44:38 +0200

    PHP - Site

--- PHP1erePartie/08-site/profil.php
+++ PHP1erePartie/08-site/profil.php
@@ -28,8 +28,28 @@
                 $contenu .= '<p>Votre ville :' . $_SESSION['membre']['ville'] . '</p>';
             $contenu .= '</div>';            
             
-            
-            
+        // Exercice : 
+        /*
+            1- Affichez le suivi des commandes du membre(s'il y en a) dans une liste <ul><li> : id_commande, date et état de la commande. S'il n'y en a pas, vous affichez "aucune commande en cours".
+
+            2- ...
+        
+        
+        */ 
+        $pdo = new PDO('mysql:host=localhost;dbname=site', 'root', '', array(PDO:: ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+        // echo '<pre>';print_r($_SESSION);'</pre>';
+            $id_membre = $_SESSION['membre']['id_membre'];
+            $suivi = executeRequete("SELECT id_commande, id_membre, montant, date_enregistrement, etat FROM commande WHERE id_membre = :id_membre", array(':id_membre'=>$id_membre));
+                
+                while( $commande = $suivi->fetch(PDO::FETCH_ASSOC)){
+                    // On affiche le suivi des commandes :
+                    $contenu .= '<p>Vous avez '. $commande['id_commande'] .'</p>';
+                    $contenu .= '<p>'. $commande['id_membre'] .'</p>';
+                    $contenu .= '<p>'. $commande['montant'] .'</p>';
+                    $contenu .= '<p>'. $commande['date_enregistrement'] .'</p>';
+                    $contenu .= '<p>'. $commande['etat'] .'</p>';
+                }
+                // $contenu .= '<p>Vous n\'avez aps de commande</p>';                
 
 
 
