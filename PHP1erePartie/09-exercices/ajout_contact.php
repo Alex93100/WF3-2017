c48ca4a6fad028d394ea7bb310d4417084931778
Etudiant <Etudiant>
2017-05-05 10:47:50 +0200

    PHP - Exercices

---
+++ PHP1erePartie/09-exercices/ajout_contact.php
@@ -0,0 +1,25 @@
+<?php
+
+/* 1- Créer une base de données "contacts" avec une table "contact" :
+	  id_contact PK AI INT(3)
+	  nom VARCHAR(20)
+	  prenom VARCHAR(20)
+	  telephone INT(10)
+	  annee_rencontre (YEAR)
+	  email VARCHAR(255)
+	  type_contact ENUM('ami', 'famille', 'professionnel', 'autre')
+
+	2- Créer un formulaire HTML (avec doctype...) afin d'ajouter un contact dans la bdd. Le champ année est un menu déroulant de l'année en cours à 100 ans en arrière à rebours, et le type de contact est aussi un menu déroulant.
+	
+	3- Effectuer les vérifications nécessaires :
+	   Les champs nom et prénom contiennent 2 caractères minimum, le téléphone 10 chiffres
+	   L'année de rencontre doit être une année valide
+	   Le type de contact doit être conforme à la liste des types de contacts
+	   L'email doit être valide
+	   En cas d'erreur de saisie, afficher des messages d'erreurs au-dessus du formulaire
+
+	3- Ajouter les contacts à la BDD et afficher un message en cas de succès ou en cas d'échec.
+
+*/
+
+
---
+++ PHP1erePartie/09-exercices/liste_contact.php
@@ -0,0 +1,9 @@
+<?php
+/*
+	1- Afficher dans une table HTML la liste des contacts avec les champs nom, prénom, téléphone, et un champ supplémentaire "autres infos" avec un lien qui permet d'afficher le détail de chaque contact.
+
+	2- Afficher sous la table HTML le détail d'un contact quand on clique sur le lien "autres infos".
+
+
+
+*/
