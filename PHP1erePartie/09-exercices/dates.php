a099dc99ecff875fc87954d638fb06781d9f7d12
Etudiant <Etudiant>
2017-05-05 17:11:29 +0200

    PHP - Exercices

--- PHP1erePartie/09-exercices/dates.php
+++ PHP1erePartie/09-exercices/dates.php
@@ -11,20 +11,31 @@
 
 function conversionDate($date, $format){
     // Version avec objet date:
-    $objetDate = new DateTime($date);
+    // $objetDate = new DateTime($date);
 
+    //     if($format == 'FR'){
+    //         return $objetDate->format('d-m-Y') . '<br>';
+    //     }
+    //     elseif($format == 'US'){
+    //         return $objetDate->format('Y-m-d') . '<br>';            
+    //     }
+    //     else{
+    //         return 'je ne connais pas le format de cette date';            
+    //     }
+    
+    // -------------------------
+        // Autre version :
         if($format == 'FR'){
-            return $objetDate->format('d-m-Y') . '<br>';
+            return strftime('%d-%m-%Y', strtotime($date)). '<br>';// strtotie retourne la date donnée en timesstamp. strftime retourne le timestamp formanté selon le format indiqué avec des %
         }
         elseif($format == 'US'){
-            return $objetDate->format('Y-m-d') . '<br>';            
+            return strftime('%Y-%m-%d', strtotime($date)). '<br>';
         }
         else{
             return 'je ne connais pas le format de cette date';            
         }
+    
     }
-
-
     echo conversionDate('05-05-2017', 'US');
     echo conversionDate('2017-05-05', 'FR');
 ?>
