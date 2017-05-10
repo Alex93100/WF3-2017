05d1a3e7b1c639f25b77c97fe4f4c6d88bdcc448
Etudiant <Etudiant>
2017-05-10 14:41:16 +0200

    PHP - Controle

--- PHP1erePartie/10-controle/controle_2.php
+++ PHP1erePartie/10-controle/controle_2.php
@@ -1,42 +1,26 @@
 <!-- Exercice 2 : On part en voyage -->
 <?php
+    $message = '';
 
-    // function conversionMoney($money, $format){
+    function conversionMoney($money, $format){
+        $taux = array(1.085965, 0.919920);
 
-    //     if($format == 'FR'){
-    //         $resultat = $format * 0.919920;
-    //         return $resultat;// strtotie retourne la date donnée en timesstamp. strftime retourne le timestamp formanté selon le format indiqué avec des %
-    //     }
-    //     elseif($format == 'US'){
+        if($format == 'FR'){
+            $formatMoney = $taux[0]; // 1.08
+        $conversionMoney = $money * $formatMoney;
+            
+        return "$conversionMoney $.";		
+            
+        }
+        else{
+            $formatMoney = $taux[1]; // 0.91
+        $conversionMoney = $money * $formatMoney;
+            
+        return "$conversionMoney €.";		
+                    
+        }
 
-    //         $resultat = $format * 1.085965;
-    //         return $resultat;
-    //     }
-    //     else{
-    //         return 'je ne connais pas le format de cette date';            
-    //     }
-    
-    // }
-    
-
-
-
-            $message = '';
-
-			function conversionMoney($money, $format){
-				$taux = array(1.085965, 0.919920);
-
-				if($format == 'FR'){
-					$formatMoney = $taux[0]; // 1.08
-				}
-				else{
-					$formatMoney = $taux[1]; // 0.91		
-				}
-
-			    $conversionMoney = $money * $formatMoney;
-
-				return "$conversionMoney.";		
-				}
+    }
     echo conversionMoney(3, 'US').'<br>';
-    echo conversionMoney(1, 'FR');
+    echo conversionMoney(2, 'FR');
 ?>
