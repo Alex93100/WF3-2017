044fef5037ea7c9d2163890b5f3b6da6c46805c8
Etudiant <Etudiant>
2017-05-15 12:49:36 +0200

    Boucle JS

--- Mars/29mars/exo47/js/script.js
+++ Mars/29mars/exo47/js/script.js
@@ -3,7 +3,6 @@ var users = [ 'Kevin', 'Marjolaine', 'Alexandre', 'Marine'];
 
 // Faire une boucle for sur le tableau pour saluer chacun des prénoms
 for(var i = 0; i<users.length; i++){
-
     // Code a exécuter à chaque itération (boucle)
     console.log('Salut ' + users[i]);
 };
