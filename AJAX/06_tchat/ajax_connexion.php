ebc1458e5863376e6a30ebdbdd026e1ec3c1bdfe
Etudiant <Etudiant>
2017-05-17 14:22:38 +0200

    Ajax

--- AJAX/06_tchat/ajax_connexion.php
+++ AJAX/06_tchat/ajax_connexion.php
@@ -15,19 +15,21 @@ $mode = isset($_POST['mode']) ? $_POST['mode'] : '';
 
 // action = condition ? condition vrai(if) : condition fausse(else)
 $pseudo = isset($_POST['pseudo']) ? $_POST['pseudo'] : '';
-$mode = isset($_POST['civilite']) ? $_POST['civilite'] : '';
-$mode = isset($_POST['vile']) ? $_POST['vile'] : '';
-$mode = isset($_POST['date_de_naissance']) ? $_POST['date_de_naissance'] : '';
+$civilite = isset($_POST['civilite']) ? $_POST['civilite'] : '';
+$ville = isset($_POST['ville']) ? $_POST['ville'] : '';
+$date_de_naissance = isset($_POST['date_de_naissance']) ? $_POST['date_de_naissance'] : '';
 
 if($mode == "connexion"){
     //  traitement de la connexion/inscription
     // requete pour tester si le pseudo est déjà présent dans la BDD
     $resultat = $pdo->query("SELECT * FROM membre WHERE pseudo = '$pseudo'");
     $membre = $resultat->fetch(PDO::FETCH_ASSOC);
-    if($membre->rowCount() == 0){ // s'il n'y a pas de ligne alors le pseudo est libre car inexistant en BDD
+    if($resultat->rowCount() == 0){ // s'il n'y a pas de ligne alors le pseudo est libre car inexistant en BDD
         
         $time = time(); // on récupère le timestamp
-        $pdo->query("INSERT INTO membre (pseudo, civilite, ville, date_de_naissance, ip, date_connexion) VALUES ('$pseudo', '$civilite', '$ville', '$date_de_naissance', '$_SERVER[REMOTE_ADDR]', $time)");
+        $pdo->query("INSERT INTO membre (pseudo, civilite, ville, date_de_naissance, ip, date_connexion) VALUES ('$pseudo', '$civilite', '$ville', '$date_de_naissance', '$_SERVER[REMOTE_ADDR]', '$time')");
+
+        $tab['resultat'] = "membre enregistré !";
 
     }
 
