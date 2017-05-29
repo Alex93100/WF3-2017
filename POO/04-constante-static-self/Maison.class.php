6171105b6da89e132ed09a2df089fd0db4d0e856
Alex93100 <rodrigues.alexandrepro@gmail.com>
2017-05-29 16:18:52 +0200

    constante-static-self

--- POO/04-constante-static-self/Maison.class.php
+++ POO/04-constante-static-self/Maison.class.php
@@ -3,3 +3,22 @@
 // 04-constante-static-self
     // -> Maison.class.php
 
+class Maison{
+
+    public $couleur = 'blanc';
+    public static $espaceTerrain ='500m2';
+
+
+
+
+
+
+
+}
+
+// ------------------
+echo 'Terrain : '. Maison::$espaceTerrain.'<br>'; // OK Je fais appel à un élément appartenant à la classe depuis la classe.
+$maison = new Maison;
+
+echo 'Couleur : ' . $maison->couleur . '<br>'; // OK! Je fais appel à un élément de l'objet par l'objet.
+// echo 'Terrain : ' . $maison->espaceterrain . '<br>'; //Erreur ! Je tente d'appeler un élément appartenant à la classe par l'objet.
