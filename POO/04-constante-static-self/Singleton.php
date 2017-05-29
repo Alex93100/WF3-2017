16bacc71da8f0d2255641ad45e6ab485f9234984
Alex93100 <rodrigues.alexandrepro@gmail.com>
2017-05-29 17:25:11 +0200

    constante-static-self

--- POO/04-constante-static-self/Singleton.php
+++ POO/04-constante-static-self/Singleton.php
@@ -4,5 +4,23 @@
 	//-> Singleton.php
 
 // Design Pattern : Littéralement "Patron de conception", est une réponse donné à un problème que rencontre plusieurs (tous) développeurs
+// Le singleton est la réponse à la question : Comment créer une classe qui ne sera instanciable qu'une seul et unqiue fois.
 
-// Le signleton est la réponse à la question : Comment créer une classe qui ne sera instanciable qu'une seul et unqiue fois.
+ class Singleton{
+    private static $instance = NULL;
+    private function __construct(){ // Fonction private donc notre classe n'est plus instanciable.
+        
+    }
+    private function __clone(){ // Fonction private donc la classe n'est pas clonable.
+
+    }
+
+    public static function getInstance(){
+        if(!self::$instance){
+            self::$instance = new Singleton; // Je mets dans la propriété $instance un objet de la classe self/Singleton
+        }
+        return self::$instance
+    }
+ }
+
+//  $singleton = new Singleton; // IMPOSSIBLE d'instancier notre classe.
