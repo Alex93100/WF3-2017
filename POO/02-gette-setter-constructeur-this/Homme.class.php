88111aa65fe499d84da7c24d2b55970945006abd
Alex93100 <rodrigues.alexandrepro@gmail.com>
2017-05-29 12:13:57 +0200

    gette-setter-constructeur-this

--- POO/02-gette-setter-constructeur-this/Homme.class.php
+++ POO/02-gette-setter-constructeur-this/Homme.class.php
@@ -7,6 +7,26 @@ class Homme{
     private $prenom; // Propriété private
     private $nom; // Propriété private
 
+    public function setPrenom($arg){
+        if(!empty($arg) && is_string($arg) && strlen($arg) > 3 && strlen($arg) < 20){
+            $this->prenom = $arg;
+        }
+    }
+
+    public function getPrenom(){
+        return $this->prenom;
+    }
+
+    public function setNom($arg){
+        if(!empty($arg) && is_string($arg) && strlen($arg) > 3 && strlen($arg) < 20){
+            $this->nom = $arg;
+        }
+    }
+
+    public function getNom(){
+        return $this->nom;
+    }
+
 }
 
 // ----------------------------
@@ -14,3 +34,9 @@ class Homme{
 $homme = new Homme;
 // $homme->prenom = 'Alexandre'; // ERREUR : Propriété private donc innaccessible à l'extérieur de la classe
 // echo $homme->prenom;
+
+$homme->setPrenom('Alexandre');
+echo 'Prénom : '. $homme-> getPrenom() . '<br>';
+
+$homme->setNom('Rodrigues');
+echo 'Nom : '. $homme-> getNom();
