1390afa8a58fa6e596f570efbb9b350a69d7ef68
Alex93100 <rodrigues.alexandrepro@gmail.com>
2017-05-29 17:14:05 +0200

    constante-static-self

---
+++ POO/04-constante-static-self/Singleton.php
@@ -0,0 +1,8 @@
+<?php
+
+//04-constante-static-self
+	//-> Singleton.php
+
+// Design Pattern : Littéralement "Patron de conception", est une réponse donné à un problème que rencontre plusieurs (tous) développeurs
+
+// Le signleton est la réponse à la question : Comment créer une classe qui ne sera instanciable qu'une seul et unqiue fois.
