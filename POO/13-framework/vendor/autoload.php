7c4dc20f70a6f1828837a193226edffd2de0c1da
Alex93100 <rodrigues.alexandrepro@gmail.com>
2017-05-31 10:53:27 +0200

    mini framwork

--- POO/13-framework/vendor/autoload.php
+++ POO/13-framework/vendor/autoload.php
@@ -3,10 +3,21 @@
 //vendor/autoload.php
 
 class Autiload{
-    public static function className(){
+    public static function className($nom_de_la_classe){
+        // $pc = new Controller\ProduitController;
+        // require 'Controller\ProduitController.php';
 
+        $tab = explode('\\', $nom_de_la_classe);
 
+        "0" => Controller
+        "1" => ProduitController
 
+        if($tab[0] == 'Controller'){
+            $path = __DIR__ . '/../src/' . implode ('/', $tab) . '.php';
+        }
+        else{
+            $path = __DIR__ . '/' . implode ('/', $tab) . '.php';
+        }
     }
 }
 
