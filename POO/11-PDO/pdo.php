afe1cfe3e963a9b1574dc2d3f2a0165da3eb55c9
Alex93100 <rodrigues.alexandrepro@gmail.com>
2017-05-31 14:41:35 +0200

    PDO

---
+++ POO/11-PDO/pdo.php
@@ -0,0 +1,5 @@
+<? 
+// $pdo = new PDO('mysql:host=localhost;dbname=entreprise', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+$pdo = new PDO('mysql:host=localhost;dbname=entreprise', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+$resultat = $pdo-> query("fdgdfdgdfdfdgdg");
