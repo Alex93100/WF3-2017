5caca0df8f1836da45db462d94e8e022e84052b7
Alex93100 <rodrigues.alexandrepro@gmail.com>
2017-05-31 10:11:02 +0200

    Explications

---
+++ POO/explications.php
@@ -0,0 +1,6 @@
+Arborescence :
+
+app/ : La configuration de l'appliction
+src/ : Le code PHP et HTML spécifique à notre projet
+vendor/ : code générale (coeur)) de l'application et les entités.
+web/ : Repertoire web racine . Le point d'entrée de notre application (index.php, img/ photo/ js/ css/ font/) (assets)
