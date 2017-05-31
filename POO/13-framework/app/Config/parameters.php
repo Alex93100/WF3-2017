8da796939216b4c0d2da3fcc0f0c51856878803c
Alex93100 <rodrigues.alexandrepro@gmail.com>
2017-05-31 10:24:23 +0200

    Perso

---
+++ POO/13-framework/app/Config/parameters.php
@@ -0,0 +1,18 @@
+<?php
+
+// app/Config/parameters.php
+
+$parameters = array(
+    "connect" => array(
+        "host" => "localhost",
+        "dbname" => "site",
+        "login" => "root",
+        "password" => "",
+    )
+);
+
+// ----------------------------------
+
+echo '<pre>';
+print_r($parameters);
+echo '</pre>';
